61aace780de6f16d79d5be20c644e24fee4b6d07
Simone D'Amico <sim@me.com>
2017-05-08 14:07:05 +0100

    Open browser

--- TruelayerDemo/ViewController.swift
+++ TruelayerDemo/ViewController.swift
@@ -19,7 +19,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func openTruelayerAuthDialog() {
+        let baseURL = "https://auth.truelayer.com/"
+        let redirectURI = "https://requestbin.t7r.co/puzrjepv"
+        let scopes = "offline_access%20info%20accounts%20transactions%20balance"
+        let clientID = "test"
+        let url = URL(string: "\(baseURL)?enable_mock=true&response_type=code&response_mode=form_post&client_id=\(clientID)&redirect_uri=\(redirectURI)&scope=\(scopes)&nonce=foo&state=bar");
+        UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+    }
 
 }
-
